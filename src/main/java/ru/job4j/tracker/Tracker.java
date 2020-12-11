6ade0f30267d54ded3b776530f83a82463771498
BogdanProkopenko1 <integer.args@gmail.com>
2020-12-11 17:10:52 +0500

    5. Tracker - хранилище [#405981]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int counter = 0;
+        Item[] defragmentation = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                defragmentation[counter] = items[i];
+                counter++;
+            }
+        }
+        defragmentation = Arrays.copyOf(defragmentation, counter);
+        return defragmentation;
+    }
+
+    public Item[] findByName(String key) {
+        int counter = 0;
+        Item[] result = new Item[items.length];
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].equals(key)) {
+                result[counter] = items[i];
+                counter++;
+            }
+        }
+        result = Arrays.copyOf(result, counter);
+        return result;
     }
 }
