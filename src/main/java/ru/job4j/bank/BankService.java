8a7a97146ea5fb5de749d2da9893c8fe8936121d
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-04 20:48:45 +0500

    1. Организовать сортировку User [#405876]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -16,28 +16,31 @@ public class BankService {
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         boolean add = true;
-        for (Account rsl : users.get(user)) {
-            String check = rsl.getRequisite();
-            if (check.contains(account.getRequisite())) {
-                add = false;
-                break;
+        if (!users.get(user).equals(null)) {
+            for (Account rsl : users.get(user)) {
+                String check = rsl.getRequisite();
+                if (check.equals(account.getRequisite())) {
+                    add = false;
+                    break;
+                }
+            }
+            if (add) {
+                List<Account> put = new ArrayList<>();
+                for (Account accounts : users.get(user)) {
+                    put.add(accounts);
+                }
+                put.add(account);
+                users.put(user, put);
             }
         }
-        if (!add) {
-            List<Account> put = new ArrayList<>();
-            for (Account accounts : users.get(user)) {
-                put.add(accounts);
-            }
-            put.add(account);
-            users.put(user, put);
-            }
     }
 
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
-            if (user.getPassport().contains(passport)) {
+            if (user.getPassport().equals(passport)) {
                 rsl = user;
+                break;
             }
         }
         return rsl;
@@ -45,10 +48,14 @@ public class BankService {
 
     public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
-        for (Account account : users.get(findByPassport(passport))) {
-            if (account.getRequisite().contains(requisite)) {
-                rsl = account;
+        if (users.get(findByPassport(passport)) != null) {
+            for (Account account : users.get(findByPassport(passport))) {
+                if (account.getRequisite().equals(requisite)) {
+                    rsl = account;
+                }
             }
+        } else {
+            return null;
         }
         return rsl;
     }
@@ -59,23 +66,27 @@ public class BankService {
         boolean rsl = false;
         User dest = findByPassport(destPassport);
         User src = findByPassport(srcPassport);
-        for (Account accountDest : users.get(dest)) {
-            if (accountDest.getRequisite().contains(destRequisite)) {
-                transfer = accountDest;
-                break;
+        if (dest != null) {
+            for (Account accountDest : users.get(dest)) {
+                if (accountDest.getRequisite().contains(destRequisite)) {
+                    transfer = accountDest;
+                    break;
+                }
             }
         }
-        for (Account accountSrc : users.get(src)) {
-            if (accountSrc.getRequisite().contains(srcRequisite)) {
-                if (amount >= accountSrc.getBalance()) {
-                    double balanceNow = transfer.getBalance() + amount;
-                    transfer.setBalance(balanceNow);
-                    rsl = true;
-                    break;
-                } else {
-                    System.out.println("The transferred amount exceeds the account balance.");
-                    rsl = false;
-                    break;
+        if (src != null) {
+            for (Account accountSrc : users.get(src)) {
+                if (accountSrc.getRequisite().contains(srcRequisite)) {
+                    if (amount >= accountSrc.getBalance()) {
+                        double balanceNow = transfer.getBalance() + amount;
+                        transfer.setBalance(balanceNow);
+                        rsl = true;
+                        break;
+                    } else {
+                        System.out.println("The transferred amount exceeds the account balance.");
+                        rsl = false;
+                        break;
+                    }
                 }
             }
         }
