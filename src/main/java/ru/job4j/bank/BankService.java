433948e934f99c672d8efbce0fae25a9ded42b9a
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-10 00:45:15 +0500

    3. Что такое JavaDoc [#405867]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -12,7 +12,11 @@ public class BankService {
     public void addUser(User user) {
         users.put(user, new ArrayList<Account>());
     }
-
+    /**
+     * Метод по паспорту находит пользователя и добавляет в список аккаунтов новый
+     * @param passport для верификации пользователя
+     * @param account новый счет пользлвателя
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -22,7 +26,11 @@ public class BankService {
             }
         }
     }
-
+    /**
+     *
+     * @param passport идентефикатор пользователя
+     * @return возврашает User с passport идентичным введенному идентефикатору
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
@@ -33,7 +41,14 @@ public class BankService {
         }
         return rsl;
     }
-
+    /**
+     *
+     * @param passport идентефикатор пользователя
+     * @param requisite идентефикатор счета пользователя
+     * Метод принимает passport, через метод findByPassport извлекаает пользователя
+     * по извлеченному пользователю возвращается коллекция с account
+     * @return возврашяет аккаунт с реквизитами идентичными введенным
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
         if (users.get(findByPassport(passport)) != null) {
@@ -46,7 +61,16 @@ public class BankService {
         }
         return rsl;
     }
-
+    /**
+     * Метод принимает пасспорт и реквизиты получателя и отправителя
+     * Нахоодит из аккаунты и переводит сумму amount получателю, снимая со счета отправителя
+     * @param srcPassport пасспорт отправителя
+     * @param srcRequisite счет отправителя
+     * @param destPassport пасспорт получателя
+     * @param destRequisite счет получателя
+     * @param amount сумма перевода
+     * @return возвращает осуществлен перевож или нет
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         Account accountSrc = findByRequisite(srcPassport, srcRequisite);
