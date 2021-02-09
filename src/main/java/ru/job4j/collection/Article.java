0f262b76fb5e9d4053b012761b50b622b8ceb6cf
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-09 22:01:13 +0500

    add files Article.java

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,9 @@
+package ru.job4j.collection;
+
+public class Article {
+
+    public static boolean generateBy(String origin, String line) {
+        //for (String original : origin.split(" ")) {}
+        return false;
+    }
+}
