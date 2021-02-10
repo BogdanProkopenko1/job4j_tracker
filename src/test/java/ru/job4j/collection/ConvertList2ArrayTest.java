006e9bee823a7e3d43c9883544b36e420a0919f9
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-11 01:30:00 +0500

    Attachment fix

--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -1,15 +1,12 @@
 package ru.job4j.collection;
 
-import org.junit.Ignore;
 import org.junit.Test;
-
 import java.util.Arrays;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-@Ignore
 public class ConvertList2ArrayTest {
+
     @Test
     public void when7ElementsThen9() {
         ConvertList2Array list = new ConvertList2Array();
