f4b21eefd42c608dcba5f1046ed8d6a10c5c4a01
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-12 19:39:51 +0500

    4. Генерация документации по JavaDoc [#405868]

---
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -0,0 +1,46 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class SearchAtt {
+    /*
+
+    public static List<Attachment> loop(List<Attachment> f) {
+
+    }
+
+    public static List<Attachment> filterSize(List<Attachment> list, int to) {
+        Function<List<Attachment>, List<Attachment>> function
+         = new BinaryOperator<List<Attachment>>() {
+            @Override
+            public List<Attachment> apply(List<Attachment> attachments,
+             List<Attachment> attachments2) {
+
+                return;
+            }
+        }
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getSize() > 100) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        for (Attachment att : list) {
+            if (att.getName().contains("bug")) {
+                rsl.add(att);
+            }
+        }
+        return rsl;
+    }
+
+ */
+}
