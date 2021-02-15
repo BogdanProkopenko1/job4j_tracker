88277363fd69e24c32ff2de1c4326b3150266cb2
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-15 19:49:38 +0500

    2.0. Встроенные функциональные интерфейсы. [#405706] fix 2

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -6,10 +6,10 @@ import java.util.function.*;
 
 public class SearchAtt {
 
-    private static List<Attachment> filter(List<Attachment> list, Predicate<String> func) {
+    private static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
         for (int index = 0; index <= list.size(); index++) {
-            if (func.test(list.get(index).getName())) {
+            if (func.test(list.get(index))) {
                 rsl.add(list.get(index));
             }
         }
@@ -17,73 +17,12 @@ public class SearchAtt {
     }
 
     public List<Attachment> FName(List<Attachment> list) {
-        Predicate<String> func = (value) -> value.equals("bug");
+        Predicate<Attachment> func = (value) -> value.getName().equals("bug");
         return filter(list, func);
     }
 
     public List<Attachment> FSize(List<Attachment> list) {
-        int size = 100;
-        Predicate<Integer> func = (value) -> value > size;
+        Predicate<Attachment> func = (value) -> value.getSize() > 100;
         return filter(list, func);
     }
-
-    /*
-
-    public static int multiplication(int to) {
-        BiFunction<Integer, Integer, Integer> func = new BinaryOperator<Integer>() {
-            @Override
-            public Integer apply(Integer left, Integer right) {
-                return left * right;
-            }
-        };
-        Supplier<Integer> initValue = new Supplier<Integer>() {
-            @Override
-            public Integer get() {
-                return 1;
-            }
-        };
-        return filter(to, func, initValue);
-    }
-
-    public static List<Attachment> filterSize(List<Attachment> list, int size) {
-        Function<Attachment, Attachment> function = new UnaryOperator<Attachment>() {
-            @Override
-            public Attachment apply(Attachment attachment) {
-                if (attachment.getSize() > initSize.get()) {
-                    return attachment;
-                } else {
-                    return null;
-                }
-            }
-            Supplier<Integer> initSize = new Supplier<Integer>() {
-                @Override
-                public Integer get() {
-                    return size;
-                }
-            };
-        };
-        return filter(list, initSize);
-    }
-
-    public static List<Attachment> filterName(List<Attachment> list, String name) {
-        Function<Attachment, Attachment> function = new UnaryOperator<Attachment>() {
-            @Override
-            public Attachment apply(Attachment attachment) {
-                if (attachment.getSize() > initSize.get()) {
-                    return attachment;
-                } else {
-                    return null;
-                }
-            }
-            Supplier<String> initName = new Supplier<String>() {
-                @Override
-                public String get() {
-                    return name;
-                }
-            };
-        };
-        return filter(list, value);
-    }
-
- */
 }
