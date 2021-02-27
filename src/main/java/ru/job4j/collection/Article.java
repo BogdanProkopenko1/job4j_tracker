3d16c24be58998b8372f56e0560d3c1435248128
BogdanProkopenko1 <integer.args@gmail.com>
2021-02-27 18:45:55 +0500

    1. Экзамен. Коллекции. [#405872] fix

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,15 +1,17 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Article {
 
     public static boolean generateBy(String origin, String line) {
-        String[] text = line.split(" ");
-        boolean rsl = true;
-        for (int i = 0; i < text.length; i++) {
-            if (!origin.contains(text[i])) {
-                rsl = false;
-            }
-        }
-        return rsl;
+        String[] originalArray = origin.split(" ");
+        String[] copiedArray = line.split(" ");
+        HashSet<String> originalSet = new HashSet<String>();
+        HashSet<String> copiedSet = new HashSet<String>();
+        originalSet.addAll(Arrays.asList(originalArray));
+        copiedSet.addAll(Arrays.asList(copiedArray));
+        return !originalSet.addAll(copiedSet);
     }
 }
